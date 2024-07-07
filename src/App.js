aede0ae9638a5bcebf878c1094d6383749ce7afb
Maisam Ali <maisamali2002@gmail.com>
2024-07-08 02:48:38 +0500

    learning

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ function App() {
     <div className="App">
       <h1>My First React App</h1>
       {/* <p>Welcome to my first React app!</p> */}
-    {/* <ColorButton /> */}
-    {/* <List /> */}
     <SearchBar />
 
     </div>
