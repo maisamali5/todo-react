b308e081ff3da5132d028849e9efb27ef665b604
Maisam Ali <maisamali2002@gmail.com>
2024-07-09 04:05:01 +0500

    task 2 of todo list

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -4,23 +4,38 @@ import "./App.css";
 
 const SearchBar = () => {
   const [ListTask, setListTask] = useState(""); 
-  const [addBox, setaddBox] = useState("");
+  const [DummyList, setDummyList] = useState('');
+ const [addBox, setaddBox] = useState("");
   const [Await , setAwait] = useState(false);
+  const [Search, setSearch] = useState(false);
+  const [NonSearch, setNonSearch]= useState(true);
 
   const handleChange = (e) => {
     setaddBox(e.target.value);
+
+    if(ListTask.length > 0 && addBox !== "") {
+      setDummyList(ListTask.filter(list => list.input.includes(addBox)));
+      setAwait(true);
+      setNonSearch(false);
+      setSearch(true);
+    }
   };
 
   const addTodo = (addBox) => {
-    if (addBox) {
+    if (addBox && ListTask.length === 0) {
+      setSearch(false);
+      setNonSearch(true);
+      setAwait(true);
       setListTask([...ListTask, { input: addBox, id: ListTask.length + 1 }]);
       setaddBox("");
-      setAwait(true); 
+      setDummyList(ListTask);
+    }else if(ListTask.filter(list => list.input.includes(addBox))){
+      alert("Task already exist");
     }
   };
 
   const deleteTask = (id) => {
-    const newList = ListTask.filter(blog => blog.id !== id);
+    const newList = ListTask.filter(list => list.id !== id);
     setListTask(newList);
 }
 
@@ -36,9 +51,24 @@ const SearchBar = () => {
       />
       <button className="btn" onClick={() => addTodo(addBox)}>Add task </button>
       
-      {Await && <div className="container"> 
+      {Await && NonSearch && <div className="container">
             <h1> All Todo task list </h1>
                 {ListTask.map((list) => (    
+                <div className="blog-preview" key={list.id}>
+                    <h3>{list.input}</h3>
+                    <input className="btn" type="button" value="Delete" onClick={()=>deleteTask(list.id)} style={
+                    {backgroundColor: 'red',color: 'white',
+                      border: 'none',padding: '10px',
+                      borderRadius: '5px',cursor: 'pointer',
+                    margin: '10px'}  
+                    } />
+                </div>
+            ))}
+        </div>}
+
+        {Await && Search && <div className="container">
+            <h1> All Todo task list </h1>
+                {DummyList.map((list) => (    
                 <div className="blog-preview" key={list.id}>
                     <h3>{list.input}</h3>
                     <input className="btn" type="button" value="Delete" onClick={()=>deleteTask(list.id)} style={
